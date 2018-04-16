72adc5a2be84400037881bdd265187260518372d
niallmc14 <niallmc14@gmail.com>
2018-04-16 20:16:24 +0100

    Update Players.java

--- Monopoly/src/Players.java
+++ Monopoly/src/Players.java
@@ -3,21 +3,47 @@ import java.util.ArrayList;
 public class Players {
 	
 	public static final int MAX_PLAYERS = 8;
-	
-	public ArrayList<Player> playersArray;
-	public boolean playerRemoved;
+	public static ArrayList<Player> playersArray;
 	
 	public Players() {
 		
 		playersArray = new ArrayList<Player>();
-		playerRemoved = false;
 	}
 	
-	public void addPlayers(Player player){
-		
-		for(int i = 0; i < playersArray.size(); i++) {
-			
-		}
+	public void addPlayers(Player player){ 
+		playersArray.add(player);
 	}
 
+	public void removePlayers(Player player) {
+		
+		boolean removed = false;
+		
+		for(Player p: playersArray) {
+			
+			if(p.getName().equals(player.getName())) {
+				playersArray.remove(player);
+				removed = true;
+				break;
+			}
+		}
+	}
+	
+	
+	public static void main(String args[]) {
+		Players p = new Players();
+		Player person = new Player("Niall");
+		Player person2 = new Player("Stephen");
+		Player person3 = new Player("Cian");
+		
+		p.addPlayers(person);
+		p.addPlayers(person2);
+		p.addPlayers(person3);
+
+		System.out.println(playersArray.toString());
+		
+		p.removePlayers(person);
+		
+		System.out.println(playersArray.toString());
+
+	}
 }
