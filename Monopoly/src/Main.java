bdb6fca027ca6784d753ed77f341cd316c63328d
niallmc14 <niallmc14@gmail.com>
2018-04-16 20:25:57 +0100

    Main file to run game

---
+++ Monopoly/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	public static void main(String args[]) {
+		
+		Panopoly p = new Panopoly();
+		
+		/*
+		 * 
+		 * Methods to be added 
+		 * 
+		 * 1.Input names
+		 * 2. Decide starter 
+		 * 3. Loop that runs until game finishs
+		 * */
+		
+	}
+}
