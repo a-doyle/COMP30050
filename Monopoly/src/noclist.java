9e7bc6677a5f77fd8cc74506ce8ad47000d2d8e9
Joel Jacob <tomboy71@gmail.com>
2018-04-30 10:35:25 +0100

    Update noclist.java

--- Monopoly/src/noclist.java
+++ Monopoly/src/noclist.java
@@ -1,5 +1,44 @@
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Random;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import java.util.Hashtable;
+
 public class noclist implements Property
 {
+	static Random DICE 						 = new Random();
 	
+	private String knowledgeDir				 = null;   // directory where knowledge-base(s) can be found
+	
+	// Various modules of the knowledge-base
+	
+	private KnowledgeBaseModule NOC          = null;
+	private KnowledgeBaseModule CATEGORIES   = null;
+	private KnowledgeBaseModule CLOTHES      = null;
+	private KnowledgeBaseModule CREATIONS    = null;
+	private KnowledgeBaseModule DOMAINS      = null;
+	private KnowledgeBaseModule WORLDS       = null;
+	private KnowledgeBaseModule VEHICLES     = null;
+	private KnowledgeBaseModule WEAPONS	     = null;
+	private KnowledgeBaseModule PLACES       = null;
+	private KnowledgeBaseModule SUPERLATIVES = null;
+	private KnowledgeBaseModule COMPARATIVES = null;
+	private KnowledgeBaseModule ANTONYMS	 = null;
+	private KnowledgeBaseModule PAST_PERFECTS= null;
+	
+	private Vector allPeople				 = null;
+	private Vector fictionalPeople			 = null;
+	private Vector realPeople				 = null;
+	private Vector men						 = null;
+	private Vector women					 = null;
+	
+	private Hashtable NEG_QUALITIES 		 = null;
+	private Hashtable POS_QUALITIES 		 = null;
+	private Hashtable ALL_QUALITIES 		 = null;
+	
+
+	private Vector attributeFields 			 = null;
+	private Vector allFields	 			 = null;
 }
