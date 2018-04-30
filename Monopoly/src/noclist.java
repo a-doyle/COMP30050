d9add7f85016aa0d92f74d55fcd931aa87eeb6eb
Joel Jacob <tomboy71@gmail.com>
2018-04-30 22:48:21 +0100

    Update noclist.java

--- Monopoly/src/noclist.java
+++ Monopoly/src/noclist.java
@@ -43,4 +43,42 @@ public class noclist implements GameBoard
 	private Vector allFields	 			 = null;
 }
 
+public noclist(String kbDirectory)
+	{
+		knowledgeDir = kbDirectory;
+		
+		NOC           = new KnowledgeBaseModule(knowledgeDir + "Veale's The NOC List.txt", 0);
+		CATEGORIES    = new KnowledgeBaseModule(knowledgeDir + "Veale's Category Hierarchy.txt", 0);
+		CLOTHES       = new KnowledgeBaseModule(knowledgeDir + "Veale's clothing line.txt", 1);  // 1 is the column number of the key value
+		CREATIONS     = new KnowledgeBaseModule(knowledgeDir + "Veale's creations.txt", 0);
+		DOMAINS       = new KnowledgeBaseModule(knowledgeDir + "Veale's domains.txt", 0);
+		WORLDS        = new KnowledgeBaseModule(knowledgeDir + "Veale's fictional worlds.txt", 0);
+		VEHICLES      = new KnowledgeBaseModule(knowledgeDir + "Veale's vehicle fleet.txt", 1);  // 1 is the column number of the key value
+		WEAPONS	      = new KnowledgeBaseModule(knowledgeDir + "Veale's weapon arsenal.txt", 1);  // 1 is the column number of the key value
+		PLACES        = new KnowledgeBaseModule(knowledgeDir + "Veale's place elements.txt", 0);		
+		SUPERLATIVES  = new KnowledgeBaseModule(knowledgeDir + "superlatives.txt", 0);
+		COMPARATIVES  = new KnowledgeBaseModule(knowledgeDir + "comparatives.txt", 0);
+		ANTONYMS	  = new KnowledgeBaseModule(knowledgeDir + "antonyms.txt", 0);
+		PAST_PERFECTS = new KnowledgeBaseModule(knowledgeDir + "past perfects.txt", 0);
+		POS_QUALITIES = NOC.getInvertedField("Positive Talking Points");
+		NEG_QUALITIES = NOC.getInvertedField("Negative Talking Points");
+		ALL_QUALITIES = NOC.getInvertedField("Positive Talking Points");
+		ALL_QUALITIES = NOC.getInvertedField("Negative Talking Points", ALL_QUALITIES);
+		
+		allPeople       = NOC.getAllFrames();
+		
+		fictionalPeople = NOC.getAllKeysWithFieldValue("Fictive Status", "fictional");
+		realPeople      = NOC.difference(allPeople, fictionalPeople);
+		
+		men			    = NOC.getAllKeysWithFieldValue("Gender", "male");
+		women			= NOC.getAllKeysWithFieldValue("Gender", "female");
+		
+		allFields		= NOC.getFieldNames();
+		
+		attributeFields = new Vector();
+		attributeFields.add("Negative Talking Points");
+		attributeFields.add("Positive Talking Points");		
+	}
+	
+
 }
