3a6f287f1e50253cb684bc33e4c3ad023d80e0af
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-05-01 12:12:11 +0100

    removed error

--- Monopoly/src/FreeParking.java
+++ Monopoly/src/FreeParking.java
@@ -1,8 +1,9 @@
 package monopoly;
 
-public class FreeParking {
+public class FreeParking implements Tile {
 	
 	private int taxCollected;
+	private static final TileType TILE_TYPE = TileType.FREE_PARKING;
 	
 	// FreeParking Constructor
 	public FreeParking(int startingAmount) {
@@ -16,5 +17,17 @@ public class FreeParking {
 	// when a player lands on FreeParking
 	public int retrieveTax() {
 		return taxCollected;
+	}
+
+	// giveMoney() needs to be implemented in Player class
+	@Override
+	public GameState hasLandedOn(Player player) {
+		player.addBalanceTransaction(taxCollected);
+		return GameState.PLAYING;
+	}
+
+	@Override
+	public TileType getTileType() {
+		return TILE_TYPE;
 	}
 }
