6bcf6ba5a1eb45278c6e183176d81d8fef569dd3
niallmc14 <niallmc14@gmail.com>
2018-05-04 20:16:59 +0100

    Update CardType.java

--- Monopoly/src/final_project/CardType.java
+++ Monopoly/src/final_project/CardType.java
@@ -1,18 +1,19 @@
 package monopoly_take2;
 
 public enum CardType {
+	
 	COMMUNITY, CHANCE;
 	
-	public static CardType parseType(String s) {
+	public static CardType parseType(int s) {
 		
-		switch (s) {
-			case "community":
-				return CardType.COMMUNITY;
-			
-			case "chance":
-				return CardType.CHANCE;
-		}
+		if(s == 2 || s == 17 || s == 33)
+			return CardType.COMMUNITY;
+		
+		else if(s == 7 || s == 22 || s == 36)
+			return CardType.CHANCE;
 		
 		return null;
 	}
+
 }
+
