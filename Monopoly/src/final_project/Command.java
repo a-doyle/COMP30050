f6db6ff5056b8e50d19d015233ee1ee596450c69
niallmc14 <niallmc14@gmail.com>
2018-05-07 20:39:39 +0100

    Committed updates

--- Monopoly/src/final_project/Command.java
+++ Monopoly/src/final_project/Command.java
@@ -1,273 +1,410 @@
-package monopoly_take2;
+package Sprint4;
 
 import java.util.ArrayList;
 import javax.swing.JTextArea;
+import java.util.Random;
 
 public class Command
 {
 	public static int roll;
-	public static JTextArea textArea = UI.textArea;
+	public static JTextArea textArea = Board.textArea;
+	public static String[] valid_instructions = {"roll", "property", "balance", "done", "buy" ,"pay rent", "quit" }; 
+	public static Dice di = new Dice();
+	public static int currPlayer;
+	public static CommunityChest ccCards = new CommunityChest();
+	
 	static void GAMEOVER(int winner)
 	{
-		textArea.append(Board.playerList[winner].name + " won the game, free supplements for life!");
-		scroll();
-		UI.commandPanel.removeAll();
+		textArea.append(Board.PlayerArray[winner].name+" Is the winner!");
+		Scroll();
+		Board.commandPanel.removeAll();
 	}
+	@SuppressWarnings("static-access")
 	
-	public static void roll()
-	{
-		int player = Board.currPlayer;
-		int position=Board.playerList[player].position;
+	public static void roll(){
 		
-		if(Board.rollable != true)
-		{
-			UI.textArea.append("Cannot roll again!\n");
-			scroll();
+		currPlayer = Board.currentPlayer;
+		int position = Board.PlayerArray[currPlayer].position;
+		boolean threeDoubles = false;
+		int diceValue1 = di.getDice1Value();
+		int diceValue2 = di.getDice2Value();
+		
+		/*Only starting must think of more logical way of determining if the user has 
+		rolled more then 2 doubles during his turn*/
+		
+//		if(Board.PlayerArray[player].rollCount > 3){
+//			Board.textArea.append("You rolled three Doubles ! \n Sadly thats not good and you must go to jail.");
+//			//Send user to jail
+//		}
+		
+		if(Board.canRoll != true){
+			Board.textArea.append("Cannot roll again!\n");
+			Scroll();
 			return;
 		}
 		
-		int dice1 = (int)(Math.random() * 6) + 1;
-		int dice2 = (int)(Math.random() * 6) + 1;
-		
-		roll = dice1 + dice2;
+		roll = di.total();
 		position += roll;
 		
-		if(position >= 40)
-		{
+		if(position >= 40) {
 			position = position - 39;
-			Board.playerList[player].balance = Board.playerList[player].balance + 200;
+			Board.PlayerArray[currPlayer].balance = Board.PlayerArray[currPlayer].balance + 200;
 			textArea.append("You Passed Go! Heres $200\n");
 		}
 		
-		Board.playerIcons[player].setLocation(Board.boardPos[position][0] + ((player % 3) * UI.uqLoc),
-				Board.boardPos[position][1] + ((player / 3) * UI.uqLoc));
+		Board.Dots[currPlayer].setLocation(Board.BoardCoord[position][0] + ((currPlayer % 3) * Board.offset),
+				Board.BoardCoord[position][1] + ((currPlayer / 3) * Board.offset));
 		
-		Board.rollable = (dice1 == dice2);
-		textArea.append("Rolled "+ dice1 +" and "+ dice2 +"\n");
-		textArea.append("You Have landed on: "+ Board.propertyList[position].propertyName +"\n Value: " +Board.propertyList[position].value+". \n Buyable: "+ Board.propertyList[position].buyable+ "\n");
-		scroll();
+		Board.canRoll = (diceValue1 == diceValue2);
+		textArea.append("Rolled "+ diceValue1 +" and "+ diceValue2 +"\n");
+		textArea.append("You Have landed on: "+ Properties.propList[position].propertyName +"\n Value: " +Properties.propList[position].value+". \n Buyable: "+ Properties.propList[position].buyable+ "\n");
+		Scroll();
 		
-		if(Board.propertyList[position].Owned == true && Board.playerList[player] != Properties.Owner && !(Board.propertyList[position].mortgaged))
+		if(Properties.propList[position].owned == true && Board.PlayerArray[currPlayer] != Properties.propList[position].Owner && !(Properties.propList[position].mortgaged))
 		{
-			textArea.append("You have landed on an owned property with rent of "+ Board.propertyList[position].Rents[Board.propertyList[position].houseCount]+". \n This has been deducted from your balance.");
+			textArea.append("You have landed on an owned property with rent of "+ Properties.propList[position].rents[Properties.propList[position].houseCount]+". \n This has been deducted from your balance.");
 			//Properties.payRent();
-			scroll();
-			UI.CanEnd = false;
+			Scroll();
+			Board.CanEnd = false;
 		}
 		else{
-			UI.CanEnd = true;
+			Board.CanEnd = true;
 		}
 		
 		//Two functions below deduct cash from the player if they land on a tax square
 		
 		if(position == 4){
-			UI.textArea.append("Goverment is looking for their tax money. They took $100 from your balance. \n");
-			Board.playerList[player].balance = Board.playerList[player].balance - 100;
-			scroll();
+			Board.textArea.append("Goverment is looking for their tax money. They took $100 from your balance. \n");
+			Board.PlayerArray[currPlayer].balance = Board.PlayerArray[currPlayer].balance - 100;
+			Scroll();
 		}
 		
 		if(position == 38){
-			UI.textArea.append("Goverment is looking for their Lux tax money. \n They took $200 from your balance. \n");
-			Board.playerList[player].balance = Board.playerList[player].balance - 200;
-			scroll();
+			Board.textArea.append("Goverment is looking for their Lux tax money. \n They took $200 from your balance. \n");
+			Board.PlayerArray[currPlayer].balance = Board.PlayerArray[currPlayer].balance - 200;
+			Scroll();
 		}
 		
 		
 		//Function must be able to deal with not allowing the player to move unless certain conditions are met
 		if(position == 30){
-			UI.textArea.append("You have commited crimes againest Monopoly and must pay the price in a cell.");
+			Board.textArea.append("You have commited crimes againest Monopoly and must pay the price in a cell.");
 			position = 10;
-			Board.playerIcons[player].setLocation(Board.boardPos[position][0] + ((player % 3) * UI.uqLoc),
-					Board.boardPos[position][1] + ((player / 3) * UI.uqLoc));
+			Board.Dots[currPlayer].setLocation(Board.BoardCoord[position][0] + ((currPlayer % 3) * Board.offset),
+					Board.BoardCoord[position][1] + ((currPlayer / 3) * Board.offset));
 		}
 		
-		// shows if balance is in minus
-		if (Board.playerList[player].balance < 0){
-			UI.CanEnd = false;
-			UI.textArea.append("You can not roll again cause your bank balance is negative \n"+
-			"You must sell some of your assets or mortgage a property");
+//		if(Board.PlayerArray[player].threeDouble == true){
+//			//Sends the user straight to jail
+//			Board.CanEnd = true;
+//		}
+		
+		//The negative balance statement
+		if(Board.PlayerArray[currPlayer].balance < 0){
+			
+			Board.CanEnd = false;
+			Board.textArea.append("You can not roll again cause your bank balance is negative \n"+
+			"You must sell some of your Assets or mortgage a property");
+			
 		}
 				
-		 Board.playerList[player].position = position;
+		Board.PlayerArray[currPlayer].position = position;
+	
 	}
 	
-	public static void endTurn() {
-		if(UI.CanEnd == false)
-		{
+	public static void EndTurn(){
+		
+		currPlayer = Board.currentPlayer;
+
+		if(Board.CanEnd == false){
 			textArea.append("Cannot end turn until rent or other debts are paid.\n");
-			scroll();
+			Scroll();
 			return;
 		}
 		
-		UI.CanEnd=false;
-		int ActivePlayer=Board.currPlayer;
-		int playerCount=Board.playerCount;
+		Board.CanEnd = false;
+		int playerCount = Board.numberPlayers;
 		
-		ActivePlayer++;
+		currPlayer++;
 		
-		if(ActivePlayer >= playerCount)
-		{
-			ActivePlayer = ActivePlayer - playerCount;
+		if(currPlayer >= playerCount){
+			currPlayer -= playerCount;
 		}
-		while(Board.playerList[ActivePlayer].inGame == false)
-		{
-			ActivePlayer++;
-			if(ActivePlayer >= playerCount)
-			{
-				ActivePlayer = ActivePlayer - playerCount;
+		
+		while(Board.PlayerArray[currPlayer].inGame == false){
+			
+			currPlayer++;
+			if(currPlayer >= playerCount){
+				currPlayer -= playerCount;
 			}
 		}
 		
-		int i = ActivePlayer;
+		int i = currPlayer;
 		i++;
 		
-		while(Board.playerList[ActivePlayer].inGame == false)
-		{
-			if(i >= playerCount)
-			{
+		while(Board.PlayerArray[currPlayer].inGame == false){
+			
+			if(i >= playerCount){
 				i = i - playerCount;
 			}
 			i++;
 		}
-		if(i == ActivePlayer)
-		{
+		if(i == currPlayer){
 			GAMEOVER(i);
 			return;
 		}
 		
-		Board.rollable=true;
-		textArea.append("Its "+Board.playerList[ActivePlayer].name+"'s turn\n");
-		scroll();
-		Board.currPlayer = ActivePlayer;
+		Board.canRoll=true;
+		textArea.append("Its "+Board.PlayerArray[currPlayer].name+"'s turn\n");
+		Scroll();
+		Board.currentPlayer = currPlayer;
 	}
 	
 	public static void property() 
 	{
 		
-		ArrayList<Properties> temp = Board.playerList[Board.currPlayer].getPropertyOwned();
+		ArrayList<Properties> temp = Board.PlayerArray[Board.currentPlayer].getPropertyOwned();
 		
 		if(temp.size() == 0)
 		{
 			textArea.append("You own nothing.\n");
-			scroll();
+			Scroll();
 		}	
 		
 		for(int i = 0; i < temp.size(); i++)
 		{
 			textArea.append("You own "+temp.get(i).propertyName+"\n");
-			scroll();
+			Scroll();
 		}
 		
 	}
 	
-	public static String help(){
+	public static String printHelp(){
 		
 		textArea.append("List of valid inputs: ");
-		
-		for(int i = 0; i < Board.validInstructions.length; i++)
-		{
-			textArea.append(( Board.validInstructions[i] + ", "));
+		for(int i = 0; i < valid_instructions.length; i++){
+			textArea.append(( valid_instructions[i] + ", "));
 		}
-		
 		textArea.append("\n");
-		scroll();
+		Command.Scroll();
 		return null;
 	}
 	
-	// this function allows for a smoother scroll of text
-	public static void scroll()
+	public static void Scroll()
 	{
+		//Call this command after every append to keep a consistent autoscroll
+		
 		textArea.setCaretPosition(textArea.getDocument().getLength());
-	}
-	
-	public static void quit()
-	{
-		int[] sum = new int[6];
-		int currentWinner = 0;
-		
-		for(int i = 0; i < Board.playerCount; i++)
-		{
-			ArrayList<Properties> temp = Board.playerList[Board.currPlayer].getPropertyOwned();
-			for(int j = 0;j < temp.size(); j++)
-			{
-				sum[i] = sum[i] + temp.get(j).value;
-			}
-			sum[i] = sum[i] + Board.playerList[Board.currPlayer].balance;
-			
-			if(sum[i] > sum[currentWinner])
-				currentWinner=i;
-			}
-		
-			textArea.append(Board.playerList[currentWinner].name+" Is the winner!");
-			scroll();
-			UI.commandPanel.removeAll();
-		
-		
 	}
 	
 	public static void mortgageProperty(String property){
 		
-		int player = Board.currPlayer;
-		int position = Board.playerList[player].position;
+		int player = Board.currentPlayer;
+		int position = Board.PlayerArray[player].position;
 		
-		if(Properties.Owner.name == Board.playerList[player].name){
-			UI.textArea.append("Mortgaged: " + Board.propertyList[position].propertyName);
-			Board.propertyList[position].mortgaged = true;
-			Board.playerList[player].getMoney(Board.propertyList[position].value / 2);
-			Command.scroll();
+		if(Properties.propList[position].Owner.name == Board.PlayerArray[player].name){
+			
+			textArea.append("Mortgaged: " + Properties.propList[position].propertyName);
+			Properties.propList[position].mortgaged = true;
+			Board.PlayerArray[player].getMoney(Properties.propList[position].value / 2);
+			Command.Scroll();
 			return;
 		}
-		else if(Board.propertyList[position].buyable == false){
-			UI.textArea.append("You do not own this property and therefore can not Mortgage it !");
+		else if(Properties.propList[position].buyable == false){
+			Board.textArea.append("You do not own this property and therefore can not Mortgage it !");
 			return;
 		}
-		else if(Board.propertyList[position].buyable == true){
-			UI.textArea.append("Mortgaged: " + Board.propertyList[position].propertyName);
-			Board.propertyList[position].mortgaged = true;
-			Board.playerList[player].getMoney(Board.propertyList[position].value / 2);
-			Command.scroll();
+		else if(Properties.propList[position].buyable == true){
+			Board.textArea.append("Mortgaged: " + Properties.propList[position].propertyName);
+			Properties.propList[position].mortgaged = true;
+			Board.PlayerArray[player].getMoney(Properties.propList[position].value / 2);
+			Command.Scroll();
 			return;
 		}
 	}
 	
 	public static void unMortgage(String property){
 		
-		int player = Board.currPlayer;
-		int position = Board.playerList[player].position;
+		int position = Board.PlayerArray[currPlayer].position;
 		
-		if(!(Properties.Owner.name == Board.playerList[player].name)){
-			UI.textArea.append("You can not unmortgage because you did mortgage this property!");
+		if(!(Properties.Owner.name == Board.PlayerArray[currPlayer].name)){
+			Board.textArea.append("You can not unmortgage because you did mortgage this property!");
 			return;
 		}
-		else if(Properties.Owner.name == Board.playerList[player].name && Board.propertyList[position].mortgaged == true){
-			UI.textArea.append("Unmortgaged: " + Board.propertyList[position].propertyName);
-			Board.propertyList[position].mortgaged = false;
-			Board.playerList[player].payment(Board.propertyList[position].value / 2);
-			Command.scroll();
+		else if(Properties.propList[position].Owner.name == Board.PlayerArray[currPlayer].name && Properties.propList[position].mortgaged == true){
+			Board.textArea.append("Unmortgaged: " + Properties.propList[position].propertyName);
+			Properties.propList[position].mortgaged = false;
+			Board.PlayerArray[currPlayer].payment(Properties.propList[position].value / 2);
+			Command.Scroll();
 			return;
 		}
 	}
 	
-	public static void drawChance(){
+	public static void checkInput() throws InterruptedException
+	{
+			currPlayer = Board.currentPlayer;
+			int position = Board.PlayerArray[currPlayer].position;
+
+			String userInput = Board.textField.getText().toLowerCase();
+			
+			String oneWord = "";
+			String manyWord = "";
+
+		    if (userInput.indexOf(' ') > -1) //
+		    { 
+		      manyWord += userInput.substring(0, userInput.indexOf(' ')).toLowerCase(); // takes first word of many.
+		      switch(manyWord.toLowerCase()){
+					case "build":
+						String buildDetail = manyWord.replaceFirst("build ", "");
+						Houses.build(buildDetail);
+						break;
+					
+					case "demolish":
+						String demolishDetail = manyWord.replaceFirst("demolish ", "");
+						Houses.demolish(demolishDetail);
+						break;
+					
+						
+					case "mortgage":
+						String mortgageDetail = manyWord.replaceFirst("mortgage", "");
+						mortgageProperty(mortgageDetail);
+						break;
+						
+					case "unmortgage":
+						String unmortgageDetail = manyWord.replaceFirst("unmortgage ", "");
+						unMortgage(unmortgageDetail);
+						break;
+						
+					default: 
+						textArea.append("Invalid input, please try \"help\" \n");
+						Command.Scroll();
+						printHelp();
+				}
+			}
+		    
+		    else {
+		      oneWord += userInput.toLowerCase(); // the first word
+		      
+		      switch(oneWord.toLowerCase()){
+				
+					case "roll": 
+						Command.roll();
+						break;
+						
+					case "property":
+						Command.property();
+						break;
+						
+					case "balance": 
+						textArea.append("You have "+ Board.PlayerArray[currPlayer].getBalance()+" dolla\n");
+						break;
+						
+					case "done":
+						Command.EndTurn();
+						break;
+						
+					case "buy":
+						textArea.append(Properties.purchase(currPlayer)+"\n");
+						break;
+					
+					case "pay rent":
+						Properties.payRent(currPlayer);
+						break;
+						
+					case "help":
+						printHelp();
+						break;
+						
+					case "draw":
+						if(position == 7 || position == 22 || position == 36) {
+							Command.drawChance();
+						}
+						break;
+						
+					case "bankrupt":
+						Command.bankrupt();
+						break;
+						
+					default: 
+						textArea.append("Invalid input, heres some that are Valid: \n");
+						Command.Scroll();
+						printHelp();
+				}
+		      }
+		    }
+	
+	public static void drawChance() {
 		
+		Random rand = new Random();
+		int  n = rand.nextInt(16) + 1;
+
+		switch(n) {
+		case 1:
+			ccCards.cc1();
+			break;
+		case 2:
+			ccCards.cc2();
+			break;
+		case 3:
+			ccCards.cc3();
+			break;
+		case 4:
+			ccCards.cc4();
+			break;
+		case 5:
+			ccCards.cc5();
+			break;
+		case 6:
+			ccCards.cc6();
+			break;
+		case 7:
+			ccCards.cc7();
+			break;
+		case 8:
+			ccCards.cc8();
+			break;
+		case 9:
+			ccCards.cc9();
+			break;
+		case 10:
+			ccCards.cc10();
+			break;
+		case 11:
+			ccCards.cc11();
+			break;
+		case 12:
+			ccCards.cc12();
+			break;
+		case 13:
+			ccCards.cc13();
+			break;
+		case 14:
+			ccCards.cc14();
+			break;
+		case 15:
+			ccCards.cc15();
+			break;
+		case 16:
+			ccCards.cc6();
+			break;
+		}
 	}
 	
-	public static void playerBankrupt() {
-		ArrayList<Properties> temp = Board.playerList[Board.currPlayer].getPropertyOwned();
-		for(int i=0;i<temp.size();i++)
-		{
-			temp.get(i).Owned=false;
-			temp.get(i);
-			Properties.Owner=null;
-			temp.get(i);
-			Properties.ownedByPlayer=null;
-			temp.get(i).mortgaged=false;
-			temp.get(i).houseCount=0;
+	public static void bankrupt() {
+		ArrayList<Properties> temp = Board.PlayerArray[currPlayer].getPropertyOwned();
+		for(int i=0;i<temp.size();i++){
+			
+			temp.get(i).owned = false;
+			temp.get(i).Owner = null;
+			temp.get(i).ownedByPlayer = null;
+			temp.get(i).mortgaged = false;
+			temp.get(i).houseCount = 0;
 		}
-		Board.playerList[Board.currPlayer].inGame=false;
-		UI.CanEnd=true;
-		textArea.append(Board.playerList[Board.currPlayer].name+" Is Out!");
-		scroll();
-		endTurn();
+		
+		Board.PlayerArray[currPlayer].inGame = false;
+		Board.CanEnd = true;
+		textArea.append(Board.PlayerArray[currPlayer].name+" Is Out!");
+		Scroll();
+		EndTurn();
 	}
 }
