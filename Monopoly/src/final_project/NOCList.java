daf2856578e90dd380f6ec55f3f71e49ca6be437
niallmc14 <niallmc14@gmail.com>
2018-05-07 19:00:44 +0100

    NocList somewhat implemented

---
+++ Monopoly/src/final_project/NOCList.java
@@ -0,0 +1,59 @@
+package monopoly_take2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
+public class NOCList {
+
+        
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		ArrayList<String> lines = new ArrayList<String>();
+		ArrayList<ArrayList<String>> newDads = new ArrayList<ArrayList<String>>();
+
+		Path filePath = Paths.get("nocListPlaces.txt");
+	    if (Files.exists(filePath)){
+
+	     File objFile = filePath.toFile();
+	     
+	     try(BufferedReader in = new BufferedReader( new FileReader(objFile))){
+	          String line = in.readLine();
+
+	          while(line != null){ // loop till you have no more lines
+	        	  
+	        	  	line.split("\t");
+	        	  	lines.add(line);
+        	  		newDads.add(new ArrayList<String>(Arrays.asList(line.split("\t"))));
+	              line = in.readLine(); // try to read another line
+	          }
+	          
+	          newDads.remove(0);
+	          
+	          for(ArrayList<String> ads : newDads) {
+	        	  	System.out.println(ads.get(0));
+	          }
+              //System.out.println(newDads);
+
+	        }
+	         catch(IOException e){
+
+	             System.out.println(e);
+	         }
+	     
+	    }
+	    else
+	    {
+	      System.out.println(filePath.toAbsolutePath() + " doesn't exist");
+	    }
+	}
+}
